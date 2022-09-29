155221d73f9fadb7c93739f6746052b07f6b65a2
Christopher Hill <cjameshill@gmail.com>
2022-09-29 10:31:02 +0000

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -6,36 +6,34 @@ import FullPageLoading from "@/components/FullPageLoading.vue";
 import PrimaryPreRegLayout from "@/components/PrimaryPreRegLayout.vue";
 import { onMounted, watch } from "vue";
 import { useStorage } from "@vueuse/core";
-import { RouterLink, RouterView, useRoute } from 'vue-router'
+import { RouterLink, RouterView, useRoute } from "vue-router";
 import { useFirebaseAuth } from "@/composables/firebase";
 const { isAuthenticated, user } = useFirebaseAuth();
 const route = useRoute();
 watch(user, (newValue) => {
-  if (isAuthenticated) {
-    window.dataLayer = window.dataLayer || [];
-    window.dataLayer.push({
-      'user_id': newValue.uid
-    })
-  }
-})
-onMounted(() => {
-  console.log("route: ", route.query);
-  useStorage("tonto-ga", route.query);
-
+    if (isAuthenticated) {
+        window.dataLayer = window.dataLayer || [];
+        window.dataLayer.push({
+            userId: newValue.uid,
+        });
+    }
 });
-
+onMounted(() => {
+    console.log("route: ", route.query);
+    useStorage("tonto-ga", route.query);
+});
 </script>
 
 <template>
-  <div class="min-h-screen">
-    <FullPageLoading></FullPageLoading>
-    <Modal></Modal>
-    <PrimaryPreRegLayout>
-      <RouterView />
-    </PrimaryPreRegLayout>
+    <div class="min-h-screen">
+        <FullPageLoading></FullPageLoading>
+        <Modal></Modal>
+        <PrimaryPreRegLayout>
+            <RouterView />
+        </PrimaryPreRegLayout>
 
-    <!-- <PrimaryFooter /> -->
-    <Alerts></Alerts>
-    <Cookies></Cookies>
-  </div>
+        <!-- <PrimaryFooter /> -->
+        <Alerts></Alerts>
+        <Cookies></Cookies>
+    </div>
 </template>
